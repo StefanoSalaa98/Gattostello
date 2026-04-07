b3c7fa50c1793cbd9c4e858eced6def6c32ab3f0
StefanoSalaa98 <sala.stefano98@gmail.com>
2026-04-07 10:21:27 +0200

    Aggiungi aimazione scroll componente Seleziona e Tessera

--- frontend/src/components/Seleziona.jsx
+++ frontend/src/components/Seleziona.jsx
@@ -1,14 +1,17 @@
 import { FaArrowUp } from "react-icons/fa";
+import AnimateOnScroll from "../hooks/AnimateOnScroll";
 
 export default function Seleziona() {
     return (
         <>
-            <div className="seleziona">
-                <FaArrowUp className="arrow" />
-                <FaArrowUp className="arrow" />
-                <FaArrowUp className="arrow" />
-                <h3>Seleziona un'opzione </h3>
-            </div>
+            <AnimateOnScroll>
+                <div className="seleziona">
+                    <FaArrowUp className="arrow" />
+                    <FaArrowUp className="arrow" />
+                    <FaArrowUp className="arrow" />
+                    <h3>Seleziona un'opzione </h3>
+                </div>
+            </AnimateOnScroll>
         </>
     )
 }
